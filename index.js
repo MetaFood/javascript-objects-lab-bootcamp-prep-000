48ef2cbd85f72ef3cf3ca6b022ae59c13c2f5824
Matthew Smyth <msmyth1030@gmail.com>
2018-05-24 21:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object
+  return object;
 }
 function deleteFromObjectByKey(object, key) {
   var newRecipe = Object.assign({}, object);
